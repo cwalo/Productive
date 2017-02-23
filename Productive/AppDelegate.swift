f20eb0621432c8dfc8a55f9cbd4d35325d166002
Corey Walo <cjwalo@gmail.com>
2017-02-22 22:48:51 -0600

    Initial Commit

---
+++ Productive/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Productive
+//
+//  Created by Corey Walo on 2/22/17.
+//  Copyright © 2017 Corey Walo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
